7c4f762b773391cfa24b80093d9f78f27ce22cf3
Tsz Kit Wong <wongt@bu.edu>
2022-06-26 16:31:42 -0400

    Create Main.java

---
+++ Java/Lists/linkedList/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Player andrewWiggins = new Player("Andrew", "Wiggins", 22);
+        Player stephCurry = new Player("Steph", "Curry", 30);
+        Player draymondGreen = new Player("Draymond", "Green", 23);
+        Player klayThompson = new Player("Klay", "Thompson", 11);
+
+        PlayerLinkedList list = new PlayerLinkedList();
+        list.addToFront(stephCurry);
+        list.addToFront(klayThompson);
+        list.addToFront(draymondGreen);
+        list.addToFront(andrewWiggins);
+
+        list.printList();
+        System.out.println("# of elements in list: " + list.getSize());
+    }
+
+}
