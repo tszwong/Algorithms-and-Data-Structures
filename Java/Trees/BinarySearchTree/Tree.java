98be509e3cc2d811a861618775e8b85270ff8ae0
Tsz Kit Wong <wongt@bu.edu>
2022-07-26 16:17:52 -0400

    Update Tree.java
    added implementation of max and min methods

--- Java/Trees/BinarySearchTree/Tree.java
+++ Java/Trees/BinarySearchTree/Tree.java
@@ -10,9 +10,33 @@ public class Tree {
         }
     }
 
+    public int min() {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        } else {
+            return root.min();
+        }
+    }
+
+    public int max() {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        } else {
+            return root.max();
+        }
+    }
+
     public void traverseInOrder() {
         if (root != null) {
             root.traverseInOrder();
         }
+    }
+
+    public TreeNode get(int value) {
+        if (root != null) {
+            return root.get(value);
+        }
+
+        return null;
     }
 }
