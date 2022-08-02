c9641ac17f714f6ddb9773351d9e681c6264471e
Tsz Kit Wong <wongt@bu.edu>
2022-08-02 18:06:38 -0400

    Update Tree.java
    implemented delete method

--- Java/Trees/BinarySearchTree/Tree.java
+++ Java/Trees/BinarySearchTree/Tree.java
@@ -31,6 +31,38 @@ public class Tree {
             root.traverseInOrder();
         }
     }
+    
+        public void delete(int value) {
+        root = delete(root, value);
+    }
+
+    private TreeNode delete(TreeNode subtreeRoot, int value) {
+        if (subtreeRoot == null) {
+            return subtreeRoot;
+        }
+
+        if (value < subtreeRoot.getData()) {
+            subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
+        } else if (value > subtreeRoot.getData()) {
+            subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+        } else {
+            // cases 1 and 2: node to delete has 0 or 1 child(ren)
+            if (subtreeRoot.getLeftChild() == null) {
+                return subtreeRoot.getRightChild();
+            } else if (subtreeRoot.getRightChild() == null) {
+                return subtreeRoot.getLeftChild();
+            }
+
+            // case 3: node to delete has 2 children
+            // replace the value in the subtreeRoot node with the smallest value from subtree
+            subtreeRoot.setData(subtreeRoot.getRightChild().min());
+
+            // delete the node that has the smallest value in the right subtree
+            subtreeRoot.setLeftChild(delete(subtreeRoot.getRightChild(), subtreeRoot.getData()));
+        }
+
+        return subtreeRoot;
+    }
 
     public TreeNode get(int value) {
         if (root != null) {
